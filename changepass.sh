5dbe961c01577fc41c8f510647a284cca7b3530d
PrivateNess.Network <support@privateness.net>
2021-04-03 11:03:18 -0400

    Update changepass.sh

--- changepass.sh
+++ changepass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILE=/srv/emercoind/emercoin.conf
-echo -n -e "Установите пароль для соединения RPC JSON к кошельку \nили нажмите [ENTER] (оставит текущий) ":
+echo -n -e "Set a password for the RPC JSON connection to the wallet \n or press [ENTER] (will keep the current one) ":
 read -s -t 15 new_pass
 echo
 old_pass=$(cat $FILE | grep "rpcpassword" | cut -d'=' -f2)
@@ -10,10 +10,10 @@ if [ "$new_pass" == "" ]; then
   exit 1
 fi
 if [ "$new_pass" == "$old_pass" ]; then
-  echo "Введеный пароль совпадает с текущим"
+  echo "The entered password matches the current one"
   exit 0
  else
-   echo -e "\e[31mПароль изменен!\e[0m"
+   echo -e "\e[31mPassword changed!\e[0m"
    path=rpcpassword=$new_pass
    sed -i "s#.*rpcpassword.*#$path#g" $FILE
    ./emercoin-cli restart
